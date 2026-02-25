d39be57acd692b7329c8a51c7aee5d49dde3e722
thewajidmengal123 <thewajidmengal@gmail.com>
2026-02-25 21:19:08 +0500

    Create page.tsx

---
+++ app/admin/bounties/page.tsx
@@ -0,0 +1,9 @@
+import { AdminBountiesContent } from "@/components/admin-bounties-content"
+
+export const metadata = {
+  title: "Admin Bounties | Reward NFT",
+}
+
+export default function AdminBountiesPage() {
+  return <AdminBountiesContent />
+}
