b6552f48ba37004bcff1d1c360b9f20d5a1bf597
thewajidmengal123 <thewajidmengal@gmail.com>
2026-03-02 06:31:35 +0500

    Create page.tsx

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,10 @@
+import { AdminUsersContent } from "@/components/admin-users-content"
+
+export const metadata = {
+  title: "Admin - Users | Reward NFT Platform",
+  description: "Manage users and view social profiles",
+}
+
+export default function AdminUsersPage() {
+  return <AdminUsersContent />
+}
