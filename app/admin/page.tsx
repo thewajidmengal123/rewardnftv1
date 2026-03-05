ec0b647b91519975d67138b9fe14ef012528e58a
thewajidmengal123 <thewajidmengal@gmail.com>
2026-03-06 00:50:40 +0500

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 
 import { CollectionAdmin } from "@/components/collection-admin"
+import PredictionManager from "@/components/admin/PredictionManager"
 
 export default function AdminPage() {
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen p-4 space-y-8">
       <CollectionAdmin />
+      
+      {/* Add Prediction Manager Section */}
+      <div className="border-t border-gray-800 pt-8">
+        <PredictionManager />
+      </div>
     </div>
   )
 }
